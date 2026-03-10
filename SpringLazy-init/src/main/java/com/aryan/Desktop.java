dd47db75be791abc582e65e57fb5edef5eae55e6
Aryan Sohani <aryan.112123@gmail.com>
2026-03-10 08:39:03 +0530

    learnt what is lazy-init and how it is used to create object in ioc container only when the object is required

---
+++ SpringLazy-init/src/main/java/com/aryan/Desktop.java
@@ -0,0 +1,16 @@
+package com.aryan;
+
+public class Desktop implements Computer {
+
+    public void compile()
+    {
+        System.out.println("These is Desktop compiling");
+    }
+
+    public Desktop()
+    {
+        System.out.println("Desktop object created");
+    }
+
+
+}
---
+++ SpringLazy-init/src/main/java/com/aryan/Laptop.java
@@ -0,0 +1,15 @@
+package com.aryan;
+
+public class Laptop implements Computer{
+
+    public void compile()
+    {
+        System.out.println("compile using laptop");
+    }
+    public Laptop()
+    {
+        System.out.println("Laptop Object created");
+    }
+
+
+}
